1b1f80f38f4816742fbcb8172188555420478cd9
realredtext <100484201+realredtext@users.noreply.github.com>
2022-02-26 11:58:58 -0500

    Create Reminders.js

---
+++ Reminders.js
@@ -0,0 +1,14 @@
+menu.addOption('Set Reminder', function() {
+    remindabout=prompt('Remind me about:') 
+}); 
+menu.addOption('Remind me', function() {    
+    alert(`Reminder: ${remindabout}`) 
+}); 
+menu.addOption('Set Reminder (2)', function() {
+    remindabout2=prompt('Remind me about:') 
+}); 
+menu.addOption('Remind me (2)', function() {
+    alert(`Reminder: ${remindabout2}`) 
+});
+
+// mmm a brick
