09f5f32e6435880c6edfd83a5603eb6b31f64b81
realredtext <100484201+realredtext@users.noreply.github.com>
2022-03-24 15:50:48 -0400

    FP forced me to make this ('._.)

---
+++ ParseNull.js
@@ -0,0 +1,9 @@
+//for fp
+function parseNull(x) {
+    let s = x.toLowerCase();
+    if(s == "null") {
+        return null
+    } else {
+        console.error(`Your input (${s}) is not null!`)
+    }
+}
