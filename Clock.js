5c48aa9701c0a3bff47635c143b7feb7360b51bb
realredtext <100484201+realredtext@users.noreply.github.com>
2022-03-25 20:50:11 -0400

    indented it so yagton wouldnt have a stroke
    e

--- Clock.js
+++ Clock.js
@@ -2,5 +2,12 @@ let Seconds = 0
 let Minutes = 0
 let Hours = 0
 setInterval(() => {
-++Seconds; w.doAnnounce(`${Hours}h ${Minutes}m ${Seconds}s`); if (Seconds>60) {++Minutes; Seconds = 0}; if (Minutes>60) {++Hours; Minutes = 0}
+    ++Seconds;
+    w.doAnnounce(`${Hours}h ${Minutes}m ${Seconds}s`);
+    if (Seconds>60) {
+        ++Minutes; Seconds = 0
+    };
+    if (Minutes>60) {
+        ++Hours; Minutes = 0
+    }
 },1000)
