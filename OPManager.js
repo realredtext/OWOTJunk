de7f9f5e82eaced87e9f0baf3884d92026212ad6
realredtext <100484201+realredtext@users.noreply.github.com>
2022-03-28 15:22:38 -0400

    Replaced Client with Operator Manager
    yagton complimented my old version :bootiful:

--- OPManager.js
+++ OPManager.js
@@ -4,7 +4,7 @@ function addOperator(newOperator) {
             e.op = true
         }
     });
-    clientChatResponse(`${newOperator} is now (OP).`)
+    addChat(null, 0, "user", "[ Operator Manager ]", `${newOperator} is now (OP).`, "[ Operator Manager ]", false, false, false, "#0033CC", getDate())
 }
 menu.addOption("Add Operator", function() {
     let operator = prompt("Who to add as OP:");
@@ -17,7 +17,7 @@ function removeOperator(user) {
             e.op = false
         }
     })
-    clientChatResponse(`${user} is no longer (OP).`)
+    addChat(null, 0, "user", "[ Operator Manager ]", `${user} is now (OP).`, "[ Operator Manager ]", false, false, false, "#0033CC", getDate())
 }
 menu.addOption("Remove Operator", function() {
     let unOperator = prompt("Who to remove from OP:");
