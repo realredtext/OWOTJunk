f16048dee7e6635a369f667f28f602d0f14b1c9e
realredtext <100484201+realredtext@users.noreply.github.com>
2022-03-30 20:41:26 -0400

    shortened Operator to OP in [ OP Manager ]

--- OPManager.js
+++ OPManager.js
@@ -4,7 +4,7 @@ function addOperator(newOperator) {
             e.op = true
         }
     });
-    addChat(null, 0, "user", "[ Operator Manager ]", `${newOperator} is now (OP).`, "[ Operator Manager ]", false, false, false, "#0033CC", getDate())
+    addChat(null, 0, "user", "[ OP Manager ]", `${newOperator} is now (OP).`, "[ OP Manager ]", false, false, false, "#0033CC", getDate())
 }
 menu.addOption("Add Operator", function() {
     let operator = prompt("Who to add as OP:");
@@ -17,7 +17,7 @@ function removeOperator(user) {
             e.op = false
         }
     })
-    addChat(null, 0, "user", "[ Operator Manager ]", `${user} is now (OP).`, "[ Operator Manager ]", false, false, false, "#0033CC", getDate())
+    addChat(null, 0, "user", "[ OP ]", `${user} is now (OP).`, "[ OP Manager ]", false, false, false, "#0033CC", getDate())
 }
 menu.addOption("Remove Operator", function() {
     let unOperator = prompt("Who to remove from OP:");
