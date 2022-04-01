9faa96661c8ff79896c13b3f38ed13fd7a22daac
realredtext <100484201+realredtext@users.noreply.github.com>
2022-03-31 21:05:38 -0400

    Delete safeAdmin.js

--- safeAdmin.js
+++
@@ -1,5 +0,0 @@
-//pick one of these two
-w.on("chatmod", function(e) {e.admin = true})
-/*e.op is not listed as that makes you vulnerable
-to malicious HTML sent in the chat*/
-w.on("chatmod", function(e) {e.staff = true})
