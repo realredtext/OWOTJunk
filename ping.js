7359ae62cf4b7f370903ca4f23daae620fb8b138
realredtext <100484201+realredtext@users.noreply.github.com>
2022-03-31 21:05:21 -0400

    Create ping.js

---
+++ ping.js
@@ -0,0 +1,7 @@
+let pingTone = new Audio("https://upload.wikimedia.org/wikipedia/commons/transcoded/5/58/Zip_tone.ogg/Zip_tone.ogg.mp3")
+
+w.on("chatmod", (e) => {
+    if(e.message.includes(`@${state.userModel.username}`)) {
+        pingTone.play()
+    }
+})
