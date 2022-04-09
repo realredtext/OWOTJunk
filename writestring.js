3b00731fd65402da2a580fcd8770e86ae843dd37
realredtext <100484201+realredtext@users.noreply.github.com>
2022-04-09 00:03:30 -0400

    Create writestring.js

---
+++ writestring.js
@@ -0,0 +1,12 @@
+function writeString(string,MoveCursor){
+    var i;
+    for (i = 0; i < string.length; i++) { 
+          var lastChar = string.charAt(i);
+          writeChar(lastChar);
+    }
+
+    for (i = 0; i < string.length; i++) { 
+    if (MoveCursor == false) { moveCursor("left"); }
+    }
+}
+//not by me, no clue who made it
