87ef808ecfc3b2e5b4dd14a23f3c5ff78eda848a
realredtext <100484201+realredtext@users.noreply.github.com>
2022-04-11 19:42:56 -0400

    updated syntax pain points

--- FPChat.js
+++ FPChat.js
@@ -2,6 +2,7 @@ function FPChat(message) {
     addChat(null, Math.round(Math.random() * 9999), "user", "fp", message, "<b>fp</b>", true, true, true, "#0000ff", getDate())
 }
 
-menu.addOption("FP Chat",function() {
-var fpMsg = prompt("Send:");FPChat(fpMsg)
+menu.addOption("FP Chat", () => {
+    var fpMsg = prompt("Send:");
+    FPChat(fpMsg)
 })
