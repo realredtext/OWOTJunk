b36d10e1bfc99ebde62bb48cb1c2b9e7e40d6d4d
realredtext <100484201+realredtext@users.noreply.github.com>
2022-04-16 03:01:27 -0400

    made it not suck

--- Slowchat.js
+++ Slowchat.js
@@ -1,7 +1,5 @@
-menu.addOption('prep', function() {
-    document.getElementById("chat_lower").remove(); 
-})
-
-menu.addOption('slowchat', function() {
-    inputtext=prompt("input text:");OWOT.chat.send(`${inputtext}`); 
+document.getElementById("chat_lower").remove()
+menu.addOption("Chat", () => {
+    let x = prompt("Send:");
+    w.chat.send(x) 
 })
