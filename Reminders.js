e33ae2fcf38c7245b98e1c9ea00d71c5e1e7a6cb
realredtext <100484201+realredtext@users.noreply.github.com>
2022-04-23 18:33:45 -0400

    updated to arrow syntax

--- Reminders.js
+++ Reminders.js
@@ -1,14 +1,14 @@
-menu.addOption('Set Reminder', function() {
+menu.addOption('Set Reminder', () => {
     remindabout=prompt('Remind me about:') 
-}); 
-menu.addOption('Remind me', function() {    
+})
+menu.addOption('Remind me', () => {    
     alert(`Reminder: ${remindabout}`) 
-}); 
-menu.addOption('Set Reminder (2)', function() {
+})
+menu.addOption('Set Reminder (2)', () => {
     remindabout2=prompt('Remind me about:') 
-}); 
-menu.addOption('Remind me (2)', function() {
+})
+menu.addOption('Remind me (2)', () => {
     alert(`Reminder: ${remindabout2}`) 
-});
+}
 
 // mmm a brick
