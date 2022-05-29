1d467c24868b508789e46dda61b83cc918ce7971
realredtext <100484201+realredtext@users.noreply.github.com>
2022-05-29 16:49:09 -0400

    Delete package_ledger.js

--- package_ledger.js
+++
@@ -1,6 +0,0 @@
-let nameArr = ["write_string", "short_hexcode", "add_manager"];
-let scriptArr = [
-    "https://cdn.jsdelivr.net/gh/realredtext/OWOTJunk@main/writestring.js", 
-    "https://cdn.jsdelivr.net/gh/realredtext/OWOTJunk@main/short_hexcode.js",
-    "https://cdn.jsdelivr.net/gh/realredtext/OWOTJunk@main/Managers.js"
-]
