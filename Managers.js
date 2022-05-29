a6aace219ed80faae2e556b1bcc431197cc608e1
realredtext <100484201+realredtext@users.noreply.github.com>
2022-05-28 20:40:29 -0400

    name is in brackets, no need to have # in color

--- Managers.js
+++ Managers.js
@@ -1,3 +1,7 @@
 function addManager(name, color, message) {
+    name = `[ ${name} ]`;
+    if(color.startsWith("#") == false) {
+        color = `#${color}`
+    }
     addChat(null, 0, "user", name, message, name, false, false, false, color, getDate())
 }
