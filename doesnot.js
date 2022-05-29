5158a8c15968cb98d736ef21c367dfc7d2b71643
realredtext <100484201+realredtext@users.noreply.github.com>
2022-05-28 20:41:48 -0400

    this should be built in to JS

---
+++ doesnot.js
@@ -0,0 +1,7 @@
+function doesNotStartWith(inputValue, searchValue) {
+    return inputValue.startsWith(searchValue) == false
+}
+
+function doesNotEndWith(inputValue, searchValue) {
+    return inputValue.endsWith(searchValue) == false
+}
