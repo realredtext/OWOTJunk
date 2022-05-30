b52eb3a249b11e56f48d50094121a098acea47ec
realredtext <100484201+realredtext@users.noreply.github.com>
2022-05-29 23:52:52 -0400

    Update rankadder.js

--- rankadder.js
+++ rankadder.js
@@ -4,5 +4,6 @@ client_commands.addrank = (args) => {
             e.dataObj.rankName = args[1];
             e.dataObj.rankColor = args[2]
         }
-    })
+    });
+    addChat(null, 0, "user", "[ Rank Manager ]", `Rank Added: ${args[0]}: ${args[1]} ${args[2]}`, "[ Rank Manager ]", false, false, false, "#DDDD00", getDate())
 }
