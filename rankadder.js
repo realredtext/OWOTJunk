e6abfaf3b9add6cd8b4f27384f19c0b78c548e90
realredtext <100484201+realredtext@users.noreply.github.com>
2022-05-29 23:49:44 -0400

    Update rankadder.js

--- rankadder.js
+++ rankadder.js
@@ -2,7 +2,7 @@ client_commands.addrank = (args) => {
     w.on("chatmod", (e) => {
         if(e.realUsername == args[0]) {
             e.dataObj.rankName = args[1];
-            e.dataObj.rankName = args[2]
+            e.dataObj.rankColor = args[2]
         }
     })
 }
