7539dbc1ade99007d9b78d1e1bcc8119af0b48a7
realredtext <100484201+realredtext@users.noreply.github.com>
2022-05-29 23:57:49 -0400

    Update ledger.js

--- ledger.js
+++ ledger.js
@@ -1,5 +1,7 @@
-let scripts = {
+ let scripts = {
     "write_string": "https://cdn.jsdelivr.net/gh/realredtext/OWOTJunk@main/writestring.js",
     "short_hexcode": "https://cdn.jsdelivr.net/gh/realredtext/OWOTJunk@main/short_hexcode.js",
-    "add_manager": "https://cdn.jsdelivr.net/gh/realredtext/OWOTJunk@main/Managers.js"
+    "add_manager": "https://cdn.jsdelivr.net/gh/realredtext/OWOTJunk@main/Managers.js",
+    "god_mode": "https://cdn.jsdelivr.net/gh/realredtext/OWOTJunk@main/Godmode.js",
+    "custom_ranks": "https://cdn.jsdelivr.net/gh/realredtext/OWOTJunk@main/rankadder.js"
 };
