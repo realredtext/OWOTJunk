d6515440cfb2ca70307cfb35c9bab47d8367a5de
Haxhillë Gjalluti <100484201+realredtext@users.noreply.github.com>
2022-06-18 15:20:36 -0400

    complete overhaul, now supports every rank
    demotions have been added, which return a user to their normal status

---
+++ promotions.js
@@ -0,0 +1,35 @@
+client_commands.promote = (args) => {
+    // /promote (user) (rank)
+    let validRanks = {
+        "staff": "staff",
+        "admin": "admin",
+        "op": "op"
+    };
+
+    let rankTags = {
+        "staff": {rankName: "M", rankColor: "#008000"},
+        "admin": {rankName: "A", rankColor: "#FF0000"},
+        "op": {rankName: "OP", rankColor: "#0033CC"}
+    }
+    if(! Object.keys(validRanks).includes(args[1])) {
+        clientChatResponse(`${args[1]} is not a valid type!`)
+    };
+    w.on("chatmod", (e) => {
+        if(e.realUsername === args[0]) {
+            e[validRanks[args[1]]] = true;
+            e.dataObj = rankTags[validRanks[args[1]]]
+        }
+    });
+    clientChatResponse(`${args[0]} promoted to ${args[1]}`)
+};
+
+client_commands.demote = (args) => {
+    w.on("chatmod", (e) => {
+        if(e.realUsername === args[0]) {
+            e.staff = false;
+            e.admin = false;
+            e.op = false
+        }
+    });
+    clientChatResponse(`${args[0]} demoted to normal user`)
+}
