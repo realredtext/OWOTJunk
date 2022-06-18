827a17df18b936f8555c26e79997bcd0041bcbfb
Haxhillë Gjalluti <100484201+realredtext@users.noreply.github.com>
2022-06-18 15:28:40 -0400

    updated to client command

--- FPChat.js
+++ FPChat.js
@@ -2,7 +2,6 @@ function FPChat(message) {
     addChat(null, 6955, "user", "fp", message, "<b>fp</b>", true, true, true, "#0000ff", getDate())
 }
 
-menu.addOption("FP Chat", () => {
-    var fpMsg = prompt("Send:");
-    FPChat(fpMsg)
-})
+client_commands.fpchat = (args) => {
+    FPChat(args[0])
+}
