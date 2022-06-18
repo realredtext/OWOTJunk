e8a8984685ffa3976945528c843cfee22dad7942
Haxhillë Gjalluti <100484201+realredtext@users.noreply.github.com>
2022-06-18 15:40:00 -0400

    didnt work in testing

--- useralarm.js
+++
@@ -1,8 +0,0 @@
-let aud = new Audio("https://cdn.discordapp.com/attachments/437380766444552202/962882758458376232/Random_Beep_Noise.....mp3")
-let rareUsers = ["fp", "InfraRaven"]
-let rareNicks = ["ay", "domo"]
-w.on("chatmod", (e) => {
-    if(rareUsers.includes(e.realUsername) || rareNicks.includes(e.nickname)) {
-        aud.play()
-    }
-})
