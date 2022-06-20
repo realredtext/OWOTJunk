fa872213b93f7f8cd12f28c6fd836e9e84fe733e
Haxhillë Gjalluti <100484201+realredtext@users.noreply.github.com>
2022-06-20 17:24:37 -0400

    Update ping.js

--- ping.js
+++ ping.js
@@ -1,8 +1,8 @@
-let pingTone = new Audio("https://upload.wikimedia.org/wikipedia/commons/transcoded/5/58/Zip_tone.ogg/Zip_tone.ogg.mp3")
+let pingTone = new Audio("https://upload.wikimedia.org/wikipedia/commons/transcoded/5/58/Zip_tone.ogg/Zip_tone.ogg.mp3");
 
 w.on("chatmod", (e) => {
     if(e.message.includes(`@${state.userModel.username}`)) {
-        pingTone.time = 0
-        pingTone.play()
-    }
-})
+        pingTone.time = 0;
+        pingTone.play();
+    };
+});
