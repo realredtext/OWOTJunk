3448bbca69060339837797e719687ce450863830
redtext <100484201+realredtext@users.noreply.github.com>
2022-06-26 17:17:49 -0400

    complete remake, uses button now

--- Slowchat.js
+++ Slowchat.js
@@ -1,5 +1,13 @@
-document.getElementById("chat_lower").remove()
-menu.addOption("Chat", () => {
-    let x = prompt("Send:");
-    w.chat.send(x) 
-})
+byId("chat_lower").remove();
+
+const chatBtn = document.createElement("button");
+
+chatBtn.innerText = "Chat";
+chatBtn.onclick = () => {
+    //a prompt is used to make this as slow as possible, this is SLOWchat for a reason
+    let msg = prompt("Send:");
+    api_chat_send(msg)
+};
+
+const upperChat = byId("chat_upper");
+upperChat.appendChild(chatBtn);
