74906620fe3c9e6a97e6a0415c8d54cabbf92628
redtext <100484201+realredtext@users.noreply.github.com>
2022-07-25 00:19:24 -0400

    Delete doesnot.js

--- doesnot.js
+++
@@ -1,7 +0,0 @@
-function doesNotStartWith(inputValue, searchValue) {
-    return ! inputValue.startsWith(searchValue)
-}
-
-function doesNotEndWith(inputValue, searchValue) {
-    return ! inputValue.endsWith(searchValue)
-}
