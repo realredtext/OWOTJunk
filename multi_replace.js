c5729b4a0bc31f0d73eaa0b8b0578a6daf773140
redtext <100484201+realredtext@users.noreply.github.com>
2022-08-03 02:14:01 -0400

    oh god this is super laggy

--- multi_replace.js
+++
@@ -1,7 +0,0 @@
-function multiReplace(string, searchVals, replaceVals) { //string, array, array
-    for(let i = 0; i < searchVals.length; i++) {
-        string = string.replaceAll(searchVals[i], replaceVals[i]);
-    };
-
-    return string;
-};
