cd59753d42844fc2626f981d9f602f029fbce837
redtext <100484201+realredtext@users.noreply.github.com>
2022-08-04 01:57:23 -0400

    Update broadcast_chat.js

--- broadcast_chat.js
+++ broadcast_chat.js
@@ -10,5 +10,5 @@ let broadcastChat = (msg) => {
     msg += "";
     let canChat = Permissions.user_matches_perm(state.userModel, state.worldModel, state.worldModel.chat_permission);
     let chatString = `${state.userModel.authenticated?state.userModel.username:(canChat?w.clientId:w.socketChannel)}: ${msg}`;
-    w.broadcastMessage(chatString);
+    w.broadcastCommand(chatString);
 };
