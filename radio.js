05c470350d46a78bbc342ed6007dc59da40ee71a
redtext <100484201+realredtext@users.noreply.github.com>
2023-01-01 05:31:01 -0500

    Create radio.js

---
+++ radio.js
@@ -0,0 +1,32 @@
+let tunedChannel = "100"; //channel you send AND receive from, big risk to separate the 2
+let messageName = `[${pickupChannel}]`
+
+client_commands.tune = ([setting]) => {
+    if(typeof parseFloat(setting) == typeof 1) {
+        tunedChannel = setting;
+        addChat(null, 0, "user", "[Radio]", `Tuned to ${setting}`, "e", 0,0,0, "#DD0000", getDate());
+    } else {
+        addChat(null, 0, "user", "[Radio]", "Invalid tuning setting!", "e", 0,0,0, "#DD0000", getDate());
+    };
+};
+
+w.broadcastReceive(0);
+w.on("cmd", (e) => {
+    if(JSON.parse(e.data)) {
+        let msg = JSON.parse(e.data); //message, frequency
+        if(tunedChannel === msg.frequency) {
+            addChat(null, 0, "user", messageName, msg.message, "e", 0,0,0, "#DD0000", getDate());
+        };
+    }
+});
+
+client_commands.bc = (args) => {
+    let message = args.join(" ");
+    const identification = state.userModel.username||("["+w.clientId+"]");
+    message = identification+": "+message;
+    w.broadcastCommand(JSON.stringify({
+        message,
+        frequency: tunedChannel
+    }), false);
+    addChat(null, 0, "user", "[Broadcast]", `Message sent`, "e", 0,0,0, "#DD0000", getDate());
+};
