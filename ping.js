584d53eb688bf9f5436d7701ee367bc95de97300
redtext <100484201+realredtext@users.noreply.github.com>
2023-01-07 18:19:51 -0500

    fixed the username issue, (@name) not @name

--- ping.js
+++ ping.js
@@ -1,7 +1,7 @@
 let pingTone = new Audio("https://upload.wikimedia.org/wikipedia/commons/transcoded/5/58/Zip_tone.ogg/Zip_tone.ogg.mp3");
 
 w.on("chatmod", (e) => {
-    if(e.message.includes(`@${state.userModel.username}`)) {
+    if(e.message.includes(`(@${state.userModel.username})`)) {
         pingTone.time = 0;
         pingTone.play();
     };
