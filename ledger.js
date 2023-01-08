fdc108ea3207daca2e832499cbb7ac1c67470730
redtext <100484201+realredtext@users.noreply.github.com>
2023-01-08 13:35:44 -0500

    Update ledger.js

--- ledger.js
+++ ledger.js
@@ -1,5 +1,4 @@
  let scripts = {
-    "write_string": "https://cdn.jsdelivr.net/gh/realredtext/OWOTJunk@main/writestring.js",
     "short_hexcode": "https://cdn.jsdelivr.net/gh/realredtext/OWOTJunk@main/short_hexcode.js",
     "add_manager": "https://cdn.jsdelivr.net/gh/realredtext/OWOTJunk@main/Managers.js",
     "god_mode": "https://cdn.jsdelivr.net/gh/realredtext/OWOTJunk@main/Godmode.js",
