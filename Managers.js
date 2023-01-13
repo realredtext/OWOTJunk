16c60bd7d40225b281d27b507d9c4e0c5b609737
redtext <100484201+realredtext@users.noreply.github.com>
2023-01-13 17:10:12 -0500

    FUCK CLASSES (js and school)

--- Managers.js
+++ Managers.js
@@ -1,15 +1,10 @@
-class Manager {
-    constructor(color, name) {
-        if(!name) return;
-        if(!color) color = "#000000";
-        this.name = `[ ${name} ]`;
-        this.color = color;
-    };
+function Manager(color, name) {
+    if(!name) return;
+    if(!color) color = "#000000";
+    this.name = `[ ${name} ]`;
+    this.color = color;
 
-    name = this.name;
-    color = this.color;
-
-    send(message, op) {
+    this.send(message, op=true) {
         addChat(null, 0, "user", this.name, message, this.name.replace(/[\[\]]/gm, ""), op, false, false, this.color, getDate())
     };
 };
