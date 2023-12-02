f7fb12ca499aa2ef05bb87c48dd3b3709f910307
redtext <100484201+realredtext@users.noreply.github.com>
2023-12-02 13:21:37 -0500

    underline, fixed security flaw

--- broadcast_chat.js
+++ broadcast_chat.js
@@ -1,11 +1,16 @@
 network.cmd_opt();
-let cmdChatColor = "#FF0000";
+
+//copies below, cant be modified after running
+const username = state.userModel.username;
+const id = w.clientID.toString(10);
+const authenticated = state.userModel.authenticated;
+const socketChannel = w.socketChannel;
 
 w.on("cmd", (e) => {
     if(e.data.startsWith("cmdc")) {
         const msgData = JSON.parse(e.data.substring(4)); //name, message, color
         console.log(`%c${msgData.name}:`,
-                    `color:${msgData.color};font-weight:bold;`,
+                    `color:${msgData.color};font-weight:bold;text-decoration:underline;`,
                     msgData.message
                    );
     };
@@ -20,11 +25,11 @@ w.on("cmd", (e) => {
         unauthenticated: socket channel
 */
 
-let broadcastChat = (msg) => {
+const broadcastChat = (msg) => {
     msg += "";
-    let cmdName = `${canChat?(state.userModel.authenticated?state.userModel.username:"["+w.clientId+"]"):(state.userModel.authenticated?state.userModel.username:w.socketChannel)}`
+    let cmdName = `${canChat?(authenticated?username:"["+id+"]"):(authenticated?username:socketChannel)}`
     w.broadcastCommand("cmdc"+JSON.stringify({
-        color: cmdChatColor,
+        color: "#"+YourWorld.Color.toString(16),
         name: cmdName,
         message: msg
     }));
