a156880d704587f7b7560b925f1a97e2c3e588b3
redtext <100484201+realredtext@users.noreply.github.com>
2023-12-02 13:10:44 -0500

    never took off

--- ledger.js
+++
@@ -1,6 +0,0 @@
- let scripts = {
-    "short_hexcode": "https://cdn.jsdelivr.net/gh/realredtext/OWOTJunk@main/short_hexcode.js",
-    "add_manager": "https://cdn.jsdelivr.net/gh/realredtext/OWOTJunk@main/Managers.js",
-    "god_mode": "https://cdn.jsdelivr.net/gh/realredtext/OWOTJunk@main/Godmode.js",
-    "custom_ranks": "https://cdn.jsdelivr.net/gh/realredtext/OWOTJunk@main/rankadder.js"
-};
