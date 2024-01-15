85e47a05a0d48eb6a8248a31b150387de8db16b4
redtext <100484201+realredtext@users.noreply.github.com>
2024-01-14 22:25:46 -0500

    SOLD TO THE F18 WITH 200 GS

---
+++ uviasids.js
@@ -0,0 +1,20 @@
+let uviasIDs = localStorage.getItem("uvias_ids") || {}; //uvias ids dont mean shit, dont abuse their reporting, theyre not dumb
+
+w.on("cmd", (data) => {
+    if(!data.id || !data.username) return;
+    if(uviasIDs[data.username]) return;
+
+    uviasIDs[data.username] = data.id;
+    localStorage.setItem("uvias_ids", JSON.stringify(uviasIDs));
+
+    clientChatResponse(`Registered ID ${data.id} as user ${data.username}`);
+});
+
+client_commands.uviasids = () => {
+    let string = "";
+    for(var i in uviasIDs) {
+        string += `${i}: ${i.id}<br>`
+    };
+
+    clientChatResponse(string);
+};
