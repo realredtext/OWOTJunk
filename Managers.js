43d313782b2f088219968a7e0fd68fc0fe5390aa
redtext <100484201+realredtext@users.noreply.github.com>
2024-04-01 22:59:00 -0400

    added abstraction

--- Managers.js
+++ Managers.js
@@ -4,9 +4,37 @@ function Manager(color, name) {
     this.name = `[ ${name} ]`;
     this.color = color;
 
-    this.send(message, op=true) {
-        addChat(null, 0, "user", this.name, message, this.name.replace(/[\[\]]/gm, ""), op, false, false, this.color, getDate())
+    this.send = function(message) {
+        addChat(null, 0, "user", this.name, message, this.name.replace(/[\[\]]/gm, ""), false, false, false, this.color, getDate())
     };
     
     return this;
+};
+
+function ManagerAbstraction(name, color, functions, keyPhrase) {
+    let top = this;
+
+    this.name = name || "Manager";
+    this.color = color || "#000000";
+    this.keyPhrase = keyPhrase.toLowerCase() || name.toLowerCase();
+    this.functions = {
+        ...functions,
+        help: function() {
+            return top.core.send(`Available subcommands: ${Object.keys(top.functions).join(", ")}`);
+        }
+    };
+
+    this.core = new Manager(this.color, this.name);
+
+    client_commands[keyPhrase] = function([subcommand, ...params]) {
+        if(subcommand === undefined) subcommand = "";
+        subcommand = subcommand.toLowerCase();
+        if(!subcommand || !(subcommand in top.functions)) {
+            return top.core.send(`Invalid subcommand "${subcommand||''}", refer to /${top.keyPhrase} help`);
+        } else if(subcommand === "help") {
+            return top.functions.help();
+        } else {
+            return top.core.send(top.functions[subcommand](...params));
+        }
+    };
 };
