cf89fec82f16e09d3e75d7b829587969f0092597
redtext <100484201+realredtext@users.noreply.github.com>
2024-04-02 12:06:07 -0400

    Update cmdrankclient.js

--- cmdrankclient.js
+++ cmdrankclient.js
@@ -5,6 +5,7 @@ let localRanks = {};
 w.on("cmd", (data) => {
 	if(!data.data.startsWith("Update_")) return;
 	let msg = JSON.parse(data.data.slice(7));
+	console.log(msg);
 
 	if(Array.isArray(msg)) {
 		localRanks[msg[0]] = [msg[1], msg[2]];
